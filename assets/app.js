f6fadb13791250a9b1cd21f3fccf14acb2d07758
Lou <57846057+lrreynolds@users.noreply.github.com>
2026-02-07 11:38:02 -0700

    Update app.js
    initial script

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,8 @@
+(() => {
+  const btn = document.getElementById("navToggle");
+  if (!btn) return;
 
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("navCollapsed");
+  });
+})();
